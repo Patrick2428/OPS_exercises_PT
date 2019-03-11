82dde4028b06aa9dc67ee7d2f9296be76b2dab77
Patrick Tietz <ptietz17@gmail.com>
2019-03-11 12:58:52 +0100

    Changes to exercise_3

--- ex03/display.c
+++ ex03/display.c
@@ -9,15 +9,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "displayFunctions.h"
+#include "unistd.h"
+#include "sys/wait.h"
 
 int main(int argc, char *argv[]) {
-  unsigned long int numOfTimes, numofChar, niceIncr;
-  char printMethod, printChar;
+  unsigned long int numOfTimes, numofChar;
+  char printMethod;
+  char printChar;
+  int counter, niceIncr;
+  pid_t iChild;
+  
   ErrCode err;
-
-  numofChar = (argc -4);
-  printf("Num of characters: %ld",numofChar);
-
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
   if(err != NO_ERR) {
@@ -25,10 +27,37 @@ int main(int argc, char *argv[]) {
   } else {
     printMethod = argv[1][0];
     numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
-    printChar = argv[3][0];
     niceIncr = strtoul(argv[3], NULL, 10); //String to unsigned long int
+
+  numofChar = (argc -4);
+  printf("Num of characters: %ld\n",numofChar);
+
+  for(counter = 0; counter < numofChar; counter++)
+    {
+      iChild  = fork();
+
+      switch(iChild){
+
+      case -1:
+	  perror("forking has failed,no child process created");
+	  exit(1);
+
+      case 0:
+	  //Child process
+	printChar = argv[counter + 4][0];
+	printf("The pid of child%d is: %d\nThe character: %c\nThe nice increment: %d\n",counter+1,getpid(), printChar, iChild*niceIncr);
+	//execl("./display", printChar, (char*) NULL);
+
+      default:
+	  //Mother process
+	  wait(NULL);
+	
+      }
+      
+    }
     
-    PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
+  
+  /* PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod*/
   }
   
   printf("\n");  // Newline at end
